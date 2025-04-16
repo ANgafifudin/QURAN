5a00649f3fbae2a09714c539dac65d1a871f3ec9
ANgafifudin <adjingafifudin@gmail.com>
2025-04-16 18:40:04 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import './App.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SearchBar from './components/SearchBar';
 import SurahPage from './pages/SurahPage';
+import TafsirPage from './pages/TafsirPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/surah/:nomor" element={<SurahPage />} />
-    </Routes>
+    <div>
+        <h1>QURAN-KU</h1>
+        <SearchBar />
+        <Routes>
+          <Route path="/surah/:nomor" element={<SurahPage />} />
+          <Route path="/tafsir/:nomor" element={<TafsirPage />} />
+        </Routes>
+      </div>
   </BrowserRouter>
 );
